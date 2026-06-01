b406f3caaeed5ca6fbe5ca65309444445e0ebbc3
Matheus Bressan <matheusbrerssan4444@gmail.com>
2026-06-01 13:05:54 -0300

    teste de váriaveis

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
 print('Hello, World!')
 print('Welcome to the world of Python programming!')
+
+print('nome: Matheus')
+print('sobrenome: bressan')
+
+print('Ano atual:', 2026) #será que o comentário aparece no github?
+
+'''
+Mais um teste de comentário, vamos ver se tu consegue visualizar isso mariana
+torres
+'''
+idade = 22
+Idade = 23
+IDADE = 24
+print(idade, Idade, IDADE)
